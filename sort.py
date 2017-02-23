c0e5c4b7ba8208ca1c82336caa58064615167292
jedelson <edelson.m.jason@gmail.com>
2017-02-22 20:55:56 -0800

    radix fixed
    bubble and radix sort work

--- sort.py
+++ sort.py
@@ -7,12 +7,13 @@ def bubble(unsorted):
                unsorted[place + 1] = unsorted[place]
                unsorted[place] = holder
         #print(unsorted)
-    print("Sorted list equals:\n",unsorted)
+    print("Sorted list with bubble sort equals:\n",unsorted)
     return (unsorted)
 bubble([89, 23, 33, 45, 10, 12, 45, 45, 45])
 bubble([67, 45, 2, 13, 1, 998])
 
 def radix(unsorted):
+    print("Unsorted list equals:\n",unsorted)
     zero = []
     one = []
     two = []
@@ -28,99 +29,118 @@ def radix(unsorted):
     while repeat:
         for num in range(len(unsorted)):
             modnum = 10 ** cycle
-            print (modnum)
+            #print (cycle)
             divisor = 10 ** (cycle-1)
-            print(divisor)
-            print (unsorted[num])
-            print (unsorted[num]%modnum)
-            print ((unsorted[num]%modnum)/(divisor))
-            if(((unsorted[num]%modnum)/(divisor)) == 0):
+            #print (unsorted[num])
+            if(int((unsorted[num]%modnum)/(divisor)) == 0):
                 zero.append(unsorted[num])
-                print ("append zero")
-            elif(((unsorted[num]%modnum)/(divisor)) == 1):
+                #print ("append zero")
+            elif(int((unsorted[num]%modnum)/(divisor)) == 1):
                 one.append(unsorted[num])
-                print ("append one")
-            elif(((unsorted[num]%modnum)/(divisor)) == 2):
+                #print ("append one")
+            elif(int((unsorted[num]%modnum)/(divisor)) == 2):
                 two.append(unsorted[num])
-                print ("append two")
-            elif((unsorted[num]%modnum)/(divisor) == 3):
+                #print ("append two")
+            elif(int((unsorted[num]%modnum)/(divisor)) == 3):
                 three.append(unsorted[num])
-                print ("append three")
-            elif((unsorted[num]%modnum)/(divisor) == 4):
+                #print ("append three")
+            elif(int((unsorted[num]%modnum)/(divisor)) == 4):
                 four.append(unsorted[num])
-                print ("append four")
-            elif((unsorted[num]%modnum)/(divisor) == 5):
+                #print ("append four")
+            elif(int((unsorted[num]%modnum)/(divisor)) == 5):
                 five.append(unsorted[num])
-                print ("append five")
-            elif((unsorted[num]%modnum)/(divisor) == 6):
+                #print ("append five")
+            elif(int((unsorted[num]%modnum)/(divisor)) == 6):
                 six.append(unsorted[num])
-                print ("append six")
-            elif((unsorted[num]%modnum)/(divisor) == 7):
+                #print ("append six")
+            elif(int((unsorted[num]%modnum)/(divisor)) == 7):
                 seven.append(unsorted[num])
-                print ("append seven")
-            elif((unsorted[num]%modnum)/(divisor) == 8):
+                #print ("append seven")
+            elif(int((unsorted[num]%modnum)/(divisor)) == 8):
                 eight.append(unsorted[num])
-                print ("append eight")
-            elif((unsorted[num]%modnum)/(divisor) == 9):
+                #print ("append eight")
+            elif(int((unsorted[num]%modnum)/(divisor)) == 9):
                 nine.append(unsorted[num])
-                print ("append nine")
+                #print ("append nine")
             else:
                 print("error: ", unsorted[num])
         emptyArray = []
-        if len(zero) == len(unsorted):
+        if (len(zero) == len(unsorted)):
             repeat = False
-        for x in zero:
-            emptyArray.append(x)#1
-            #zero.remove(x)
-            print(x," removed")
-        print (zero)
-        for x in one:
-            emptyArray.append(x)#2
-            one.remove(x)
-            #print(x," removed")
-        print (one)
-        for x in two:
-            emptyArray.append(x)#3
-            #two.remove(x)
-            print(x," removed")
-        print (two)
-        for x in three:
-            emptyArray.append(x)#4
-            #three.remove(x)
-            print(x," removed")
-        print (three)
-        for x in four:
-            emptyArray.append(x)#5
-            #four.remove(x)
-            print(x," removed")
-        print(four)
-        for x in five:
-            emptyArray.append(x)#6
-            #five.remove(x)
-            print(x," removed")
-        print(five)
-        for x in six:
-            emptyArray.append(x)#7
-            #six.remove(x)
-            print(x," removed")
-        print(six)
-        for x in seven:
-            emptyArray.append(x)#8
-            #seven.remove(x)
-            print(x," removed")
-        print(seven)
-        for x in eight:
-            emptyArray.append(x)#9
-            #eight.remove(x)
-            print(x," removed")
-        print(eight)
-        for x in nine:
-            emptyArray.append[x]#10
-            #nine.remove[x]
-            print(x," removed")
-        print (nine)
-        print (emptyArray)
+        if (len(zero)!= 0):
+            for x in zero:
+                emptyArray.append(x)#1
+                #zero.remove(x)
+                #print(x," removed")
+            #print (zero)
+        if (len(one)!= 0):
+            for x in one:
+                emptyArray.append(x)#2
+                #one.remove(x)
+            #print (one)
+        if (len(two)!= 0):
+            for x in two:
+                emptyArray.append(x)#3
+                #two.remove(x)
+                #print(x," removed")
+            #print (two)
+        if (len(three)!= 0):
+            for x in three:
+                emptyArray.append(x)#4
+                #three.remove(x)
+                #print(x," removed")
+            #print (three)
+        if (len(four)!= 0):
+            for x in four:
+                emptyArray.append(x)#5
+                #four.remove(x)
+                #print(x," removed")
+            #print(four)
+        if (len(five)!= 0):
+            for x in five:
+                emptyArray.append(x)#6
+                #five.remove(x)
+                #print(x," removed")
+            #print(five)
+        if (len(six)!= 0):
+            for x in six:
+                emptyArray.append(x)#7
+                #six.remove(x)
+                #print(x," removed")
+            #print(six)
+        if (len(seven)!= 0):
+            for x in seven:
+                emptyArray.append(x)#8
+                #seven.remove(x)
+                #print(x," removed")
+            #print(seven)
+        if (len(eight)!= 0):
+            for x in eight:
+                emptyArray.append(x)#9
+                #eight.remove(x)
+                #print(x," removed")
+            #print(eight)
+        if (len(nine)!= 0):
+            for x in nine:
+                emptyArray.append(x)#10
+                #nine.remove[x]
+                #print(x," removed")
+            #print (nine)
+        zero.clear()
+        one.clear()
+        two.clear()
+        three.clear()
+        four.clear()
+        five.clear()
+        six.clear()
+        seven.clear()
+        eight.clear()
+        nine.clear()
         unsorted = emptyArray        
         cycle += 1
-        print(unsorted)
-radix([67, 45, 2, 13, 1, 998,34,55,100,101,22,15,1142,1104,2,3])
+        #print(unsorted)
+    print("Sorted list with radix sort equals:\n",unsorted)
+radix([67, 45, 2, 13, 1, 998])
+radix([89, 23, 33, 45, 10, 12, 45, 45, 45])
+
+
