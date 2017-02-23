ccb2827dac24b5e5f9440a39aaf55a6f069100d9
jedelson <edelson.m.jason@gmail.com>
2017-02-22 16:31:22 -0800

    Drill 46
    uploaded Drill 46
    3 parts compleated

---
+++ range.py
@@ -0,0 +1,10 @@
+print ("Drill 1:")
+for x in range(4):
+    print (x)
+print ("Drill 2:")
+for x in range(3,-1,-1):
+    print (x, end=" ")
+print ("\nDrill 3:")
+for x in range(8,0,-2):
+    print (x, end=" ")
+    
